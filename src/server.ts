4e43aece15ca32c31ef0adfa796caaeca77472ba
tonnon <lucastonnon@gmail.com>
2022-09-21 15:25:49 -0300

    procfile

--- src/server.ts
+++ src/server.ts
@@ -7,7 +7,7 @@ import { converMinutesToHourString } from './utils/convert-minutes-to-hour-strin
 const app = express()
 app.use(express.json())
 app.use(cors())
-const prisma = new PrismaClient()
+const prisma = new PrismaClient({ datasources: {  db: { url: "file:../src/database/db.sqlite" } } })
 
 app.get('/games', async (request, response) => {
     const games = await prisma.game.findMany({
@@ -78,5 +78,3 @@ app.get('/ads/:id/discord', async (request, response) => {
 })
 
 app.listen(3333)
-
-// see
